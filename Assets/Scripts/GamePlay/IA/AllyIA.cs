7c6cb0c7cfaef95efcd3b99e770d366df012bd3f
MiguelGonzalez <valcion25562783@gmail.com>
2024-10-20 02:15:02 -0400

    Add new states to ally IA
    Add (FollowPlayer, MovingToPos) States

--- Assets/Scripts/GamePlay/IA/AllyIA.cs
+++ Assets/Scripts/GamePlay/IA/AllyIA.cs
@@ -1,3 +1,5 @@
+using Gameplay.Controllers;
+using System;
 using UnityEngine;
 using VG.IA;
 
@@ -5,11 +7,40 @@ namespace Gameplay.IA
 {
 	public class AllyIA : AstarIA
 	{
+		private GameObject playerRef;
+
+		public override void Awake()
+		{
+			base.Awake();
+
+			StateMachine.Initialize(FollowPlayerState);
+
+			//RaycastInputController.Instance.OnMoveInput += OnMoveInput;
+		}
+
 		public override void Start()
 		{
 			base.Start();
 
-			destinationSetter.target = GameObject.FindGameObjectWithTag("Player").transform;
+			playerRef = GameObject.FindGameObjectWithTag("Player");
+
+			//destinationSetter.target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+
+		public override void MoveToPoint(Vector3 point)
+		{
+			destinationSetter.target = null;
+			base.MoveToPoint(point);
+		}
+
+		private void OnMoveInput(Vector3 vector)
+		{
+			aiPath.destination = vector;
+		}
+
+		public void MoveToPlayer() 
+		{
+			destinationSetter.target = playerRef.transform;
 		}
 	}
 }
---
+++ Assets/VG/IA/Scripts/StateMachine/States/FollowPlayerState.cs
@@ -0,0 +1,49 @@
+using Gameplay.Controllers;
+using Gameplay.IA;
+using System;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace VG.IA.StateMachine
+{
+	public class FollowPlayerState : State
+	{
+		public FollowPlayerState(IA iA, StateMachineController stateMachine) : base(iA, stateMachine)
+		{
+
+		}
+
+		public override void EnterState()
+		{
+			base.EnterState();
+			iA.MoveToLastSeenPoint();
+
+			RaycastInputController.Instance.OnMoveInput += OnMoveInput;
+		}
+
+		public override void ExitState()
+		{
+			base.ExitState();
+
+			RaycastInputController.Instance.OnMoveInput -= OnMoveInput;
+		}
+
+		public override void FrameUpdate()
+		{
+			base.FrameUpdate();
+			(iA as AllyIA)?.MoveToPlayer();
+		}
+
+		public override void PhysicUpdate()
+		{
+			base.PhysicUpdate();
+		}
+
+		private void OnMoveInput(Vector3 vector)
+		{
+			iA.MoveToPoint(vector);
+			iA.StateMachine.ChangeState(iA.MovingToPosState);
+		}
+	}
+}

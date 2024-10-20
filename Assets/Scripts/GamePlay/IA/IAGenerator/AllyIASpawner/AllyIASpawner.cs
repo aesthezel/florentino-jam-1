3e00f3740fe85bf3d5cd76c92d2aa92a0968f0ce
MiguelGonzalez <valcion25562783@gmail.com>
2024-10-20 17:15:22 -0400

    AllyIA Fix
    Fix pathfinding
    Add tomb particles

--- Assets/Scripts/GamePlay/IA/IAGenerator/AllyIASpawner/AllyIASpawner.cs
+++ Assets/Scripts/GamePlay/IA/IAGenerator/AllyIASpawner/AllyIASpawner.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using VG.IA;
 
 public class AllyIASpawner : IAGenerator
 {
+	[SerializeField] private float cooldown = 10;
+
+	public Action OnActivateSpawner;
+	public Action OnDisableSpawner;
+	public Action OnSpawnIA;
+
+	private bool spawnerEnabled;
+
     public override void Start()
     {
-        
-    }
+		OnDisableSpawner?.Invoke();
+
+		StartCoroutine(SpawnerCooldown());
+	}
 
 	public override void GenerateIA()
 	{
@@ -15,6 +27,29 @@ public class AllyIASpawner : IAGenerator
 
 	public void OnMouseDown()
 	{
-		GenerateIA();
+        if (spawnerEnabled)
+        {
+			for (int i = 0; i < spawnCount; i++)
+			{
+				GenerateIA();
+
+				OnSpawnIA?.Invoke();
+
+				StartCoroutine(SpawnerCooldown());
+			}
+		}
+	}
+
+	IEnumerator SpawnerCooldown() 
+	{
+		OnDisableSpawner?.Invoke();
+
+		spawnerEnabled = false;
+
+		yield return new WaitForSeconds(cooldown);
+
+		OnActivateSpawner?.Invoke();
+
+		spawnerEnabled = true;
 	}
 }
